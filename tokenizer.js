d919a2940070c2cc91bfa97b4654950a2ea5ca3e
Chris Lau <chrislyc.lau@gmail.com>
2024-10-25 22:25:21 -0600

    exporting variable matcher

--- tokenizer.js
+++ tokenizer.js
@@ -1,52 +1,52 @@
-export class Tokenizer{
-  _matches = null;
-  /**
-   * @param {string}
-  */
-  tokenize(code){
-    const matcher = this.createMatcher();
-    this._matches = code.matchAll(matcher);
-    
-  }
-  /**
-   * @returns {string | null}
-  */
-  next(){
-    const match = this._matches.next().value;
-    if(typeof match?.[0] === "string"){
-      return match[0];
+export const variableMatcher = String.raw`([\$@%]?\w+(::\w+)?)`;
+export class Tokenizer {
+    _matches = null;
+    /**
+     * @param {string}
+     */
+    tokenize(code) {
+        const matcher = this.createMatcher();
+        this._matches = code.matchAll(matcher);
     }
-    return null;
-  }
-  createMatcher(){
-    const variableMatcher = String.raw`([\$@%]?\w+(::\w+)?)`;
-    const operatorMatcher = "(" + [
-      "<=>", 
-      "=>", 
-      String.raw`\+=`, 
-      "-=", 
-      "==", 
-      "&&", 
-      "=~", 
-      "=!",
-      String.raw`\|\|`,
-      "!=",
-      "<=", 
-      ">=", 
-      "->",
-      String.raw`s\/`,
-      String.raw`m\/`,
-      ].join("|") + ")";
+    /**
+     * @returns {string | null}
+     */
+    next() {
+        const match = this._matches.next().value;
+        if (typeof match?.[0] === "string") {
+            return match[0];
+        }
+        return null;
+    }
+    createMatcher() {
+        const operatorMatcher =
+            "(" +
+            [
+                "<=>",
+                "=>",
+                String.raw`\+=`,
+                "-=",
+                "==",
+                "&&",
+                "=~",
+                "=!",
+                String.raw`\|\|`,
+                "!=",
+                "<=",
+                ">=",
+                "->"
+            ].join("|") +
+            ")";
 
-    const spaceMatcher = String.raw`\s+`;
-    const newlineMatcher = String.raw`\n`;
-    const fullMatcher = [
-      variableMatcher,
-      operatorMatcher,
-      newlineMatcher,
-      spaceMatcher,
-      "."
-      ].join("|");
-    return fullMatcher;
-  }
+        const spaceMatcher = String.raw`\s+`;
+        const newlineMatcher = String.raw`\n`;
+        const fullMatcher = [
+            variableMatcher,
+            operatorMatcher,
+            newlineMatcher,
+            spaceMatcher,
+            "."
+        ].join("|");
+        return fullMatcher;
+    }
 }
